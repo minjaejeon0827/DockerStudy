31a05d42201a7e6108a52e18af3e8afabcf48c32
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2025-06-30 17:56:23 +0900

    [코딩애플] 도커 Docker & Container 13강 - Docker compose 3. 실시간 개발, graceful shutdown

--- docker/server.js
+++ docker/server.js
@@ -43,11 +43,48 @@ const express = require('express');   // express 라이브러리 사용
 const app = express();   
 
 // express 라이브러리 이용해서 localhost:8080 주소에 웹서버 띄우기
-app.listen(8080, () => {
+const server = app.listen(8080, () => {
   console.log('서버 실행중 http://localhost:8080');
 });
 
 // Rest API - 사용자가 메인페이지로 들어오면 '안녕' 이라는 글자 보내기
 app.get('/', (req, res) => {
-  res.send('안녕');
+  res.send('안녕하쇼11111111');
 }); 
+
+
+// * graceful shutdown 기능
+
+// 컨테이너 끄고 재시작하고 그런게 너무 느린경우들이 있다.
+// 특히 웹서버하나 끄는데 10초넘게 걸린다면 그 이유는 서버 꺼주는 코드를 안짜서 그런 것일 뿐이다.
+
+// ▲ 원래 docker 엔진이 컨테이너를 종료시킬 때 무슨 일이 일어나냐면
+// "좋은말할 때 알아서 끄라" 이런 식으로 메세지를 컨테이너의 프로그램으로 보낸다.
+// 그래서 프로그램은 그 경고성 메세지를 수신하면 알아서 종료하라고 코드짜놓으면 된다.
+// 근데 종료하는 코드가 없거나 그러면 10초 정도 기다렸다가 강제로 꺼진다.
+// 그래서 종료하는 코드를 좀 작성해보도록 하자.
+
+// 그래서 웹서버에 SIGTERM 메세지가 들어오면 server.close()하라고 코드짰다.
+
+// - express의 기본 기능인 server.close() 를 사용하면 서버에 들어온 요청을 다 처리한 후에 이쁘게 알아서 꺼진다.
+// - SIGTERM은 Docker 엔진이 컨테이너 종료할 때 보내는 "좋은 말할 때 알아서 끄라" 경고메세지이다.
+// - SIGINT는 유저가 터미널에서 ctrl + c 누를 때 보내지는 메세지이다.
+// - 참고로 터미널에 kill 1 입력하는 것도 SIGTERM 메세지 보내는 것이다.
+
+// 근데 나중에 서버에서 DB도 쓰고 그러면 서버를 끌 때 DB 연결 해제하는 것도 좋다.
+// 이런 식으로 서버에서 끄기 전에 이거저거 챙기는걸 멋있는 말로 graceful shutdown 이라고 한다.
+
+// 그래서 컨테이너로 돌릴 프로그램들은 자주 껐다켜지고 그럴 수 있으니까
+// 컨테이너 안의 프로그램들에 graceful shutdown 잘해놨나 확인하는 것도 좋은 습관이니까 나중에 한번 챙겨보자.
+process.on('SIGTERM', () => {   // 웹서버에 SIGTERM 메세지가 들어오면 
+  // TODO: DB 연결 해제 코드 필요시 구현 (2025.06.30 minjae)
+
+  server.close(() => {          // server.close() 호출하여 서버 종료 (터미널에 kill 1 입력하는 것도 SIGTERM 메세지 보내는 것이다.)
+    console.log('HTTP server closed')
+  })
+})
+process.on('SIGINT', () => {   // 웹서버에 SIGINT 메시지가 들어오면(SIGINT란? 유저가 터미널에서 ctrl + c 누를 때 보내지는 메세지)
+  server.close(() => {         // server.close() 호출하여 서버 종료 
+    console.log('HTTP server closed')   
+  })
+}) 
