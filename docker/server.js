f1535ba49b6e4c67454bdcea822451bdafe0a808
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2025-07-31 08:54:17 +0900

    [코딩애플] 도커 Docker & Container 2강 - Docker 설치 & 이미지와 컨테이너 개념 3강 - Docker로 띄워볼 서버만들기

--- docker/server.js
+++ docker/server.js
@@ -8,13 +8,13 @@
 // 6강 - 성능을 위한 Dockerfile 작성법
 
 
-// express 라이브러리 설치 순서 
+// * express 라이브러리 설치 순서 
 // 1) VSCode 터미널 실행 -> 명령어 "npm init -y" 입력 및 엔터 
 // 2) 1)번 실행 후 VSCode 터미널에 명령어 "npm install express" 입력 및 엔터 
 // 3) express 라이브러리 설치 완료 -> 폴더 "node_modules"에 express 라이브러리 소스코드 담겨있음.
 // package.json 파일은 설치한 라이브러리를 기록하는 용도의 파일이다.
 
-// nodemon 라이브러리
+// * nodemon 라이브러리
 // 특징 - 개발할 때 코드를 수정해도 바로바로 변경된 사항 반영 가능 
 // 설치 순서 
 // 1) VSCode 터미널 실행 -> 명령어 "npm install -g nodemon" 입력 및 엔터 
@@ -22,8 +22,7 @@
 // 서버(server.js) 실행 명령어
 // nodemon server.js
 
-// npm으로 뭔가 입력하는게 잘 안되는 경우
-
+// * npm으로 뭔가 입력하는게 잘 안되는 경우
 // - Node.js 설치된거 지우고 최근에 나온 LTS 버전으로 다시 설치해봅시다.
 // - 맥의 경우 sudo를 넣어서 sudo npm 어쩌구~ 입력해봅시다.
 // - 윈도우는 허가되지 않은 스크립트 어쩌구 에러가 나면 
@@ -31,7 +30,7 @@
 // Set-ExecutionPolicy unrestricted 입력하고 터미널 껐다켜서 다시 시도해봅시다.
 // - 윈도우는 onedrive 폴더 안에 코딩하면 원래 잘 안됩니다.
 
-// Dockerfile 이란?
+// * Dockerfile 이란?
 // image 파일 레시피를 적는 곳이다.
 // 아래 처럼 작성하고 docker build 명령어 실행하면 image 파일 생성 끝
 // (예) 
